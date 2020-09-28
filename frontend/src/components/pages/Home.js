3a814bcee30fc0b256281b1a3d20fdded7b861ff
charlesdowd <charlie.dowd96@gmail.com>
2020-09-28 19:41:48 +0800

    UI changes to home page

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
-import { Container } from 'react-bootstrap'
+import { Container, Button, Row, Col } from 'react-bootstrap';
+
 
 
 export default function Home() {
     const { userData } = useContext(UserContext)
+    const history = useHistory()
 
     // logged in home screen
     if (userData.user) {
@@ -20,13 +23,29 @@ export default function Home() {
 
     // logged out home page
     return (
-        <Container style={{ height: '300px', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-            <br />
-            <div style={{ textAlign: 'center' }}>
-                <h2 >Welcome to Charlie's User Authentication Practice</h2>
-                <br />
-                <p>Please Register or Log in to be authenticated!</p>
-            </div>
+        <Container style={{ height: '500px', alignItems: 'center', justifyContent: 'center', paddingTop: '100px' }}>
+            <Row>
+                <Col>
+                    <br />
+                    <div style={{ textAlign: 'center' }}>
+                        <h2 >Welcome to Charlie's User Authentication Practice</h2>
+                        <br />
+                        <h3>Please Register or Log in</h3>
+                        <br />
+                    </div>
+                </Col>
+            </Row>
+            <Row className='d-flex justify-content-center' style={{ paddingTop: '80px' }}>
+                
+                <Col sm={3} className='d-flex justify-content-center'>
+                    <Button style={{ width: '80%', height: '50px' }} onClick={() => history.push('/login')}>Login</Button>
+                </Col>
+
+                <Col sm={3} className='d-flex justify-content-center'>
+                    <Button style={{ width: '80%', height: '50px' }} onClick={() => history.push('/register')} >Register</Button>
+                </Col>
+
+            </Row>
         </Container>
     )
 }
